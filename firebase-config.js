a63f6c610131607789ea11c8bc583fb1788f62d4
Nexus <115223523+Nexus-016@users.noreply.github.com>
2025-08-01 22:32:55 +0600

    Update firebase-config.js
    Now, This Project is public!

--- firebase-config.js
+++ firebase-config.js
@@ -9,14 +9,14 @@
 (function() {
   // Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyAtFiyXzaSr8D7bvgxcJ74FI-ouvE9lxIg",
-    authDomain: "voice-for-palestine-by-nex.firebaseapp.com",
-    projectId: "voice-for-palestine-by-nex",
-    storageBucket: "voice-for-palestine-by-nex.firebasestorage.app",
-    messagingSenderId: "1050986196348",
-    appId: "1:1050986196348:web:44b91612eea60baea988f9",
-    measurementId: "G-BC9ZS8VLFK",
-    databaseURL: "https://voice-for-palestine-by-nex-default-rtdb.asia-southeast1.firebasedatabase.app"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+    databaseURL: ""
   };
 
   // Check if Firebase is loaded
